f61139fcba2c2d312fc0c9634cea1b5093e7d6e4
Dexmachi <232001371@aluno.unb.br>
2024-10-21 15:02:42 -0300

    respondido Resolucao1.java

--- aula2/fga0158/Resolucao1/Resolucao1.java
+++ aula2/fga0158/Resolucao1/Resolucao1.java
@@ -14,14 +14,14 @@ public class Resolucao1 {
 	public static void main(String[] args) {
 
 		//Declaracao de variáveis
-		Scanner **variavel** = new Scanner(System.in);
-		String **variavel** = **qual comando colocar aqui?**;
+		Scanner sc = new Scanner(System.in);
+		String nome = sc.nextLine();
 
 		//saída em modo texto
-		System.out.println("Olá " + **qual variavel colocar aqui?** + ", Seja bem vindo!");
+		System.out.println("Olá " + nome + ", Seja bem vindo!");
 
 		//Saída em modo gráfico
-		String **variavel** = JOptionPane.showInputDialog("Qual o seu nome?: ");
-		JOptionPane.showMessageDialog(null, "Olá " + **qual variavel colocar aqui?** + ", Seja bem vindo!");
+		String quadradin = JOptionPane.showInputDialog("Qual o seu nome?: ");
+		JOptionPane.showMessageDialog(null, "Olá " + nome + ", Seja bem vindo!");
 	}
 }
